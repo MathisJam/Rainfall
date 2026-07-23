d7238b414b02c8205fd7a8458f52d34b42bc5b0f
mjameau <mjameau@student.42.fr>
2026-07-23 11:29:26 +0200

    Update level0

---
+++ level0/source.c
@@ -0,0 +1,32 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int res = 0;
+    (void)argc;
+    char *args[2];
+
+
+    res = atoi(argv[1]);
+    if (res != 423)
+    {
+        fwrite("No!\n", 1, 4, stderr);
+    }
+    else
+    {
+        args[0] = strdup("/bin/sh");
+        args[1] = NULL;
+
+        gid_t gid = getegid();
+        uid_t uid = geteuid();
+        setresuid (uid, uid, uid);
+        setresgid (gid, gid, gid);
+
+        execv("/bin/sh", args);
+    }
+}
